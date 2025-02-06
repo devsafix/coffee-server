f9bda203be4828efbc337826a65b7ae479b4b3cc
Robiul <mrhrobiul008877@gmail.com>
2025-02-06 17:28:40 +0600

    Send coffee data and store in the mongodb server

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nyauv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri);
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,7 +26,15 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');
+
+
+    app.post('/coffee', async(req,res ) =>{
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
