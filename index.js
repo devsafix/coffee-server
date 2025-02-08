4638c6ca3139a395d7911ca92c443ebff9e5984a
Robiul <mrhrobiul008877@gmail.com>
2025-02-08 15:53:00 +0600

    Update single coffee information

--- index.js
+++ index.js
@@ -34,7 +34,13 @@ async function run() {
       res.send(result);
     })
 
-   
+    
+    app.get('/coffee/:id',async(req, res ) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    })
 
     app.post('/coffee', async(req,res ) =>{
       const newCoffee = req.body;
@@ -43,6 +49,25 @@ async function run() {
       res.send(result);
     })
 
+    app.put('/coffee/:id', async(req, res )=>{
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id)}
+      const options = { upsert: true };
+      const updatedCoffee = req.body;
+      const  coffee = {
+        $set:{
+          name: updatedCoffee.name,
+          chef: updatedCoffee.chef,
+          supplier:updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category:updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo  
+        }
+      }
+      const result = await coffeeCollection.updateOne(filter, coffee, options );
+      res.send(result);
+    })
 
     app.delete('/coffee/:id', async(req, res ) =>{
       const id = req.params.id;
